d3555a36b8d49c51c80d4c17803c35db2226f4d0
ShrHang <shhshrhang@gmail.com>
2026-06-28 15:06:29 +0800

    打印机卷轴打印配方的JEI显示

--- src/main/java/com/shrhang/shhs_create_core/compat/create_enchantment_industry/CreateEnchantmentIndustry.java
+++ src/main/java/com/shrhang/shhs_create_core/compat/create_enchantment_industry/CreateEnchantmentIndustry.java
@@ -1,10 +1,12 @@
 package com.shrhang.shhs_create_core.compat.create_enchantment_industry;
 
 import com.shrhang.shhs_create_core.compat.create_enchantment_industry.fluid.printing.ScollPrintingBehaviour;
-import net.neoforged.bus.api.SubscribeEvent;
+import com.shrhang.shhs_create_core.compat.create_enchantment_industry.integration.jei.category.printing.ScollPrintingRecipeJEI;
+import mezz.jei.api.registration.IRecipeRegistration;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerAboutToStartEvent;
 import plus.dragons.createenchantmentindustry.common.fluids.printer.behaviour.PrintingBehaviour;
+import plus.dragons.createenchantmentindustry.integration.jei.category.printing.PrintingCategory;
 
 public class CreateEnchantmentIndustry {
     private static boolean isRegistered = false;
@@ -13,8 +15,11 @@ public class CreateEnchantmentIndustry {
         NeoForge.EVENT_BUS.addListener(CreateEnchantmentIndustry::onServerStart);
     }
 
-    @SubscribeEvent
-    public static void onServerStart(final ServerAboutToStartEvent event) {
+    public static void jei(IRecipeRegistration registration) {
+        registration.addRecipes(PrintingCategory.TYPE, ScollPrintingRecipeJEI.listAll());
+    }
+
+    private static void onServerStart(final ServerAboutToStartEvent event) {
         if (!isRegistered) {
             PrintingBehaviour.register(ScollPrintingBehaviour::create);
             isRegistered = true;
