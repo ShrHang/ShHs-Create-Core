69fa12a4433b975e3f268ed0786bf15fef6db24c
Dinwlooc <785574259@qq.com>
2026-07-02 19:03:02 +0800

    优化恶意吸收器HostilityAbsorber： -将组合渲染调整为和喷洒器一样的模式，现在可以渲染出石磨轮。

--- src/main/java/com/shrhang/shhs_create_core/content/hostility/absorber/HostilityAbsorberRenderer.java
+++ src/main/java/com/shrhang/shhs_create_core/content/hostility/absorber/HostilityAbsorberRenderer.java
@@ -1,26 +1,29 @@
 package com.shrhang.shhs_create_core.content.hostility.absorber;
 
-import org.jetbrains.annotations.NotNull;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.AllPartialModels;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
 import net.createmod.catnip.render.CachedBuffers;
 import net.createmod.catnip.render.SuperByteBuffer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 
-/**
- * 恶意吸收器的渲染器，直接复用石磨的齿轮模型（MILLSTONE_COG），实现旋转动画。
- */
 public class HostilityAbsorberRenderer extends KineticBlockEntityRenderer<HostilityAbsorberBlockEntity> {
-    public HostilityAbsorberRenderer(@NotNull BlockEntityRendererProvider.Context context) {
+    public HostilityAbsorberRenderer(BlockEntityRendererProvider.Context context) {
         super(context);
     }
-    /**
-     * 返回旋转后的模型，使用石磨齿轮部分模型。
-     */
     @Override
-    @NotNull
-    protected SuperByteBuffer getRotatedModel(@NotNull HostilityAbsorberBlockEntity be, @NotNull BlockState state) {
-        return CachedBuffers.partial(AllPartialModels.MILLSTONE_COG, state);
+    protected void renderSafe(@NotNull HostilityAbsorberBlockEntity be, float partialTicks, @NotNull PoseStack ms,
+                              @NotNull MultiBufferSource buffer, int light, int overlay) {
+        // 1. 调用父类渲染（如果有其它模型需要）
+        super.renderSafe(be, partialTicks, ms, buffer, light, overlay);
+        // 2. 手动获取齿轮模型并渲染
+        BlockState state = be.getBlockState();
+        SuperByteBuffer gearBuffer = CachedBuffers.partial(AllPartialModels.MILLSTONE_COG, state);
+        standardKineticRotationTransform(gearBuffer, be, light)
+                .renderInto(ms, buffer.getBuffer(RenderType.solid()));
     }
 }
